7f04b9a36ba4cffb94c5ab6a0358f4ba045bf9c0
Osama Sayed <osamasayedmuhammad@gmail.com>
2025-08-08 00:13:51 +0300

    style: handle header styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Dashboard from './pages/dashboard';
 import Layout from './components/layout';
 import Header from './components/layout/header';
 import Sidebar from './components/layout/sidebar';
-import Footer from './components/layout/footer';
+// import Footer from './components/layout/footer';
 
 import './styles/scss/style.scss';
 
@@ -12,10 +12,10 @@ function App() {
     <Layout>
       <Sidebar />
 
-      <div>
+      <div className='u-flex u-flex-col'>
         <Header />
         <Dashboard />
-        <Footer />
+        {/* <Footer /> */}
       </div>
     </Layout>
   );
--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,5 +1,11 @@
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className='header u-border-b'>
+      <div className='header__content'>
+        <h1 className='header__title'>Cell Tower Network Dashboard</h1>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
