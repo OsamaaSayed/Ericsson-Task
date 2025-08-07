593664295a7a9f85e7daa22e0a87aecaf04f0fa0
Osama Sayed <osamasayedmuhammad@gmail.com>
2025-08-07 23:38:08 +0300

    style: handle sidebar styles

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,9 +1,7 @@
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className='u-container u-py-4'>
-      <div className='u-border u-border-rounded-md'>
-        <div className='layout'>{children}</div>
-      </div>
+      <div className='layout u-border u-border-rounded-md'>{children}</div>
     </main>
   );
 };
--- src/components/layout/sidebar/index.tsx
+++ src/components/layout/sidebar/index.tsx
@@ -1,5 +1,32 @@
+import Logo from './components/logo';
+import DashboardIcon from './components/dashboard-icon';
+
 const Sidebar = () => {
-  return <aside>Sidebar</aside>;
+  return (
+    <aside className='sidebar u-border-r'>
+      <div className='sidebar__header u-border-b'>
+        <a
+          className='sidebar__link'
+          href='#'
+        >
+          <Logo />
+        </a>
+      </div>
+
+      <div className='sidebar__content'>
+        <p>Home</p>
+
+        <ul>
+          <li className='sidebar__item'>
+            <a href='#'>
+              <DashboardIcon />
+              Dashboard
+            </a>
+          </li>
+        </ul>
+      </div>
+    </aside>
+  );
 };
 
 export default Sidebar;
