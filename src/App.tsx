f866ef49ef5b7b745e5b9c6389c74d5ba147ee72
Osama Sayed <osamasayedmuhammad@gmail.com>
2025-08-09 05:09:52 +0300

    chore: restructure layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
-import Dashboard from './pages/dashboard';
-
 import Layout from './components/layout';
-import Header from './components/layout/header';
-import Sidebar from './components/layout/sidebar';
+import Dashboard from './pages/dashboard';
 
 import './styles/scss/style.scss';
 
 function App() {
   return (
     <Layout>
-      <Sidebar />
-
-      <div className='u-flex u-flex-col u-flex-1 u-overflow-x-auto'>
-        <Header />
-        <Dashboard />
-      </div>
+      <Dashboard />
     </Layout>
   );
 }
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,7 +1,17 @@
+import Sidebar from './sidebar';
+import Header from './header';
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className='u-container u-py-4 u-min-h-screen'>
-      <div className='layout u-border u-border-rounded-md'>{children}</div>
+      <div className='layout u-border u-border-rounded-md'>
+        <Sidebar />
+
+        <div className='u-flex u-flex-col u-flex-1 u-overflow-x-auto'>
+          <Header />
+          {children}
+        </div>
+      </div>
     </main>
   );
 };
